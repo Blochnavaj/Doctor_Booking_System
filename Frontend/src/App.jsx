f1ac2679650dbd6173601bc80b2b6d46dbc7030d
navaj bloch <navajbloch420@gmail.com>
2024-10-19 16:06:58 +0530

    create all pages of websites

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,37 @@
- import React from 'react'
-import Navbar from './Components/Navbar'
- 
- function App() {
-   return (
-      <>
-      <Navbar/>
-      </>
-   )
- }
- 
- export default App
+import React from 'react';
+import Navbar from './Components/Navbar'; // Keep this import
+import { Route, Routes } from 'react-router-dom';
+// import Home from './Pages/Home';
+// import About from './Pages/About';
+import Doctor from './Pages/Doctor';
+import Login from './Pages/Login';
+import MyAppointments from './Pages/MyAppointments';
+import MyProfile from './Pages/MyProfile';
+import Contact from './Pages/Contact';
+import SignUp from './Pages/SignUp';
+import Appointments from './Pages/Appointments';
+import NotFound from './Pages/NotFound';
+// import Footer from './Components/Footer'; // Keep this import
+
+function App() {
+  return (
+    <>
+      <div className='mx-4 sm:mx-[10%]'>
+        <Navbar />
+        <Routes>
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/doctor' element={<Doctor />} />
+          <Route path='/doctor/:speciality' element={<Doctor />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/my-appointment' element={<MyAppointments />} />
+          <Route path='/my-profile' element={<MyProfile />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/appointments/:docId' element={<Appointments />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
